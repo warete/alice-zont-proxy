2792e959ed03263eab2460a65f73a6dd6f6d4a64
Egor Borisovsky <mr-warete@mail.ru>
2020-01-05 02:45:10 +0400

    Использование нового класса для работы с АПИ Зонта

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv,
-	GuzzleHttp\Client;
+	AliceZontProxy\ZontClient;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$client = new Client([
-	'base_uri' => getenv('ZONT_API_URL'),
-	'auth' => [getenv('ZONT_LOGIN'), getenv('ZONT_PASSWORD')],
-	'headers' => [
-		'X-ZONT-Client' => getenv('ZONT_EMAIL'),
-	],
-	'debug' => getenv('DEBUG') == "true" ? true : false,
-]);
+$zontClient = new ZontClient(
+	getenv('ZONT_API_URL'),
+	getenv('ZONT_LOGIN'),
+	getenv('ZONT_PASSWORD'),
+	getenv('ZONT_EMAIL'),
+	getenv('DEVICE_ID'),
+	getenv('DEBUG') == "true" ? true : false
+);
