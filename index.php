1e9c4922d582954ea8bec10e5811b6e2727fc16b
Egor Borisovsky <mr-warete@mail.ru>
2020-01-05 15:37:23 +0400

    Проверка наличия значения в вебхуке и выполнение действий в зависимости от пришедшего значения.

--- index.php
+++ index.php
@@ -20,3 +20,38 @@ $zontClient = new ZontClient(
 	getenv('DEVICE_ID'),
 	getenv('DEBUG') == "true" ? true : false
 );
+
+$payload = json_decode(file_get_contents("php://input"), true);
+
+if (count($payload) && isset($payload['value']))
+{
+	switch (intval($payload['value']))
+    {
+    	//Открыть машину
+		case 1:
+			$zontClient->setGuardState(false);
+			break;
+		//Закрыть машину
+		case 2:
+			$zontClient->setGuardState(true);
+			break;
+		//Завести машину
+		case 3:
+			$zontClient->setEngineState(true);
+			break;
+		//Заглушить машину
+		case 4:
+			$zontClient->setEngineState(false);
+			break;
+		//Логируем на всякий случай неизвестные значения
+		default:
+			$tempFile = fopen(__DIR__ . '/log/alice_zont_unknown_values_log.log', 'a');
+			fwrite(
+			    $tempFile,
+			    __FILE__ . ':' . __LINE__ . PHP_EOL . '(' . date('Y-m-d H:i:s').')' . PHP_EOL
+			    . print_r($payload, TRUE)
+			    . PHP_EOL . PHP_EOL
+			);
+			fclose($tempFile);
+    }
+}
